fa4d3805ff6f0e64a63f4a91b0d0958bbf91d7ea
Christophersanchez1 <122641788+Christophersanchez1@users.noreply.github.com>
2023-08-21 16:11:36 -0500

    first commit

---
+++ Question1.java
@@ -0,0 +1,6 @@
+public class Question1{
+
+    public static void main(String[] args) {
+        System.out.println(" I'm afraid for the calendar. Its days are numbered.");
+    }
+}
---
+++ Question2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(n);
+    }
+    
+}
